99dd78a604702427fc4cf57b701d83d7e926708d
Benjamin Jaeger <bjaeger2@gmail.com>
2012-02-26 20:33:12 -0500

    i h8 u zach

---
+++ authorPredictor.py
@@ -0,0 +1,80 @@
+'''
+Created on Feb 26, 2012
+New
+@author: Benjamin
+'''
+from ngrams import *
+
+def perplexity(text, model):
+    text_freq = text.get_frequencies()
+    model_prob = model.get_probabilities()
+    model_prob.smooth()
+    
+    def log_probability(acc, ngram):
+        prob = 0
+        if ngram in model_prob:
+            prob = math.log( model_prob[ngram])
+        elif model.has_tokens(ngram):
+            prob = math.log( model_prob["<zero_freq>"])
+        return acc + text_freq[ngram] * prob
+    
+    product = reduce (log_probability, text_freq, 0.0)
+    return math.pow(product, - math.e / text.get_num_tokens() )
+   #is_ngram()
+   #get_zero_freq_prob()
+   #smooth() 
+    
+#See textbook page 122
+def email_prediction(filename):
+        train_data = []
+        with open(filename) as fp:
+                train_data = fp.readlines()
+        #concatenate emails from same author
+        author_texts = {}
+        for line in train_data:
+                #author is the first word, email is the rest
+                author, email = line.split(' ',1)
+                if author in author_texts:
+                        author_texts[author] += email
+                else:
+                        author_texts[author] = email
+        #generate unigram model for each author
+        #unigram_probs = nested dict of author -> unigram -> probability
+        unigrams_probs = {}
+        for author in author_texts.iterkeys():
+                unigrams = Unigram(author_texts[author])
+                unigrams_probs[author] = unigrams.getProb()
+
+        #read in validation or train emails
+        validate_data = []
+        with open(filename) as fp:
+                validate_data = fp.readlines()
+
+        predicted_author = []
+        for line in validate_data:
+                _, email = line.split(' ',1)
+                #generate unigram model on just that email and find words that occur only once (singletons)
+                unigrams = Unigram(email)
+                #singletons = array of unigrams
+                singletons = filter( lambda x : unigrams[x]==1, unigrams.getFreq() )
+                #for each author, compute perplexity of singletons
+                #find author with max perplexity
+                max_perplexity = 0
+                max_author = None
+                for author in author_texts.iterkeys():
+                        #find the probabilities (in the known author text) of the singletons (in the unknown text)
+                        singleton_probs = {}
+                        for singleton in singletons:
+                                singleton_probs[singleton] = unigrams_probs[author][singleton];
+                        perplexity =  perplexity(singleton_probs)
+                        if (perplexity > max_perplexity) :
+                                max_perplexity = perplexity
+                                max_author = author
+                predicted_author.append(max_author)
+        return predicted_author
+
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ ngrams.py
@@ -0,0 +1,226 @@
+import nltk, math, random
+
+filename = "../Proj1Data/test.txt"
+
+def is_punct(char):
+    return char == "!" or char == "?" or char == "."
+
+def add_sentence_markers(tokens):
+        #Need to double-check
+        tokens_with_punct = ["."]
+        for word in tokens:
+            if len(word) > 1 and \
+                (word[-1:] == "!"  or word[-1:] == "?" or word[-1:] == "."):
+                tokens_with_punct.append(word[:-1])
+                tokens_with_punct.append(word[-1:])
+            else:
+                tokens_with_punct.append(word)
+        # Treat beginning and end of text as a beginning or end of sentence,
+        # respectively
+        end_token = tokens_with_punct[-1:][0]
+        if end_token != "." and end_token != "!" and end_token != "?":
+            tokens_with_punct.append(".")
+        return tokens_with_punct
+    
+class Unigram():
+    def __init__(self, filename=None, text_string=None):
+        #Reads in text of specified file
+        if filename != None:
+            with open(filename) as fp:
+                self.text = fp.read()
+        else:
+            if text_string == None:
+                print "Error: Need either filename or string"
+                return 
+            else:
+                self.text = text_string
+        # self.tokens = add_sentence_markers()
+        self.tokens = nltk.wordpunct_tokenize(self.text)
+        self.num_words = float(len(self.tokens))
+        
+        self.frequencies = None
+        self.unigrams = None
+        
+    def get_num_tokens(self):
+        return self.tokens
+    
+    def has_tokens(self, gram):
+        freqs = self.get_frequencies()
+        return gram in freqs
+    
+    def get_frequencies(self):
+        if self.frequencies != None:
+            return self.frequencies
+        else:
+            self.frequencies = dict()
+            for word in self.tokens:
+                if word in self.frequencies:
+                    self.frequencies[word] += 1.
+                else:
+                    self.frequencies[word] = 1.
+            return self.frequencies
+    
+    def get_probabilities(self):
+        if self.unigrams != None:
+            return self.unigrams
+        else:
+            frequencies = self.get_frequencies()
+            self.unigrams = dict()
+            for word in frequencies:
+                self.unigrams[word] = frequencies[word] / self.num_words
+            return self.unigrams
+            
+    def next_word(self):
+        ran = random.uniform(0,1)
+        uni_freqs = self.get_frequencies()
+        for word in uni_freqs: 
+            ran -= self.get_probability(word)
+            if ran <= 0:
+                return word
+        return "ERROR"
+    """
+        num = random.randint(0,len(self.tokens)-1)
+        next = self.tokens[num]
+        return next
+    """
+        
+    def generate_sentence(self):
+        cur_word = self.next_word()
+        sentence = ""
+        while not is_punct(cur_word):
+            sentence += cur_word + " "
+            cur_word = self.next_word()
+        return sentence[:-1] + cur_word
+    """
+        sentence = ""
+        for i in range(sentence_length):
+            sentence += self.next_word(self)
+    """
+    
+    def get_probability(self, unigram):
+        probs = self.get_probabilities()
+        if unigram not in probs:
+            return 0.
+        return probs[unigram] 
+        
+class Bigram():
+    def __init__(self, filename=None, text_string=None):
+        if filename != None:
+            with open(filename) as fp:
+                self.text = fp.read()
+        else:
+            if text_string == None:
+                print "Error: Need either filename or string"
+                return 
+            else:
+                self.text = text_string
+                
+        self.tokens = add_sentence_markers(nltk.wordpunct_tokenize(self.text))
+        self.num_words = float(len(self.tokens))
+        self.smoothed = False
+        self.uni_frequencies = None
+        self.bi_frequencies = None
+        self.bigrams = None
+        
+    def get_num_tokens(self):
+        return len(self.tokens)
+    
+    def has_tokens(self, gram):
+        (first, second) = gram
+        (uni_freqs, bi_freqs) = self.get_frequencies()
+        return first in uni_freqs and second in uni_freqs
+        
+    def get_frequencies(self):
+        if self.uni_frequencies == None or self.bi_frequencies == None:
+            # Still need to figure out beginning-of-sentence markers
+            l = len(self.tokens)
+            end_punct = self.tokens[-1:][0]
+            self.uni_frequencies = dict()
+            self.bi_frequencies = dict()
+            # Disregard final period
+            for i in range(l):
+                unigram_token = (self.tokens[i],)
+                if(i != l-1):
+                    #bigram frequencies
+                    bigram_token = (self.tokens[i], self.tokens[i+1])
+                    if bigram_token in self.bi_frequencies:
+                        self.bi_frequencies[bigram_token] += 1.
+                    else:
+                        self.bi_frequencies[bigram_token] = 1.
+                        
+                #unigram frequencies
+                if unigram_token in self.uni_frequencies:
+                    self.uni_frequencies[unigram_token] += 1.
+                else:
+                    self.uni_frequencies[unigram_token] = 1.
+            self.uni_frequencies[(end_punct,)] -= 1.
+        return (self.uni_frequencies, self.bi_frequencies)
+        
+    def get_probabilities(self):
+        if self.bigrams != None:
+            return self.bigrams
+        else:
+            (uni_frequencies, bi_frequencies) = self.get_frequencies()
+            self.bigrams = dict()
+            for (first, second) in bi_frequencies:
+                self.bigrams[(first, second)] = \
+                    bi_frequencies[(first,second)] / uni_frequencies[(first,)]
+            return self.bigrams
+
+    def smooth(self):
+        (uni_freqs, bi_freqs) = self.get_frequencies()
+        for key in bi_freqs:
+            bi_freqs[key] += 1.
+        for key in uni_freqs:
+            uni_freqs[key] += len(uni_freqs)
+        self.bigrams = None
+        self.get_probabilities()
+        self.smoothed = True
+        
+    def get_probability(self, bigram):
+        (first, second) = bigram
+        (uni_freqs, bi_freqs) = self.get_frequencies()
+        probs = self.get_probabilities()
+        if bigram not in bi_freqs and not self.smoothed:
+            return 0.
+        elif bigram not in bi_freqs and self.smoothed:
+            # Deal with unknowns here
+            if (first,) not in uni_freqs or (second,) not in uni_freqs:
+                return 0.
+            else: 
+                return 1./uni_freqs[(first,)]
+        else:
+            return probs[bigram]
+    
+    def next_word(self, prev_word):
+        ran = random.uniform(0,1)
+        (uni_freqs, bi_freqs) = self.get_frequencies()
+        for (word,) in uni_freqs: 
+            ran -= self.get_probability((prev_word, word))
+            if ran <= 0:
+                return word
+        return "ERROR"
+        
+    def generate_sentence(self):
+        cur_word = self.next_word(".")
+        sentence = ""
+        while not is_punct(cur_word):
+            sentence += cur_word + " "
+            cur_word = self.next_word(cur_word)
+        return sentence[:-1] + cur_word
+        
+        
+
+b = Bigram(filename="data/areeb.train")
+b.smooth()
+"""
+summ = 0.
+for (word,) in b.uni_frequencies:
+    summ += b.get_probability((word,"."))
+print summ"""
+print b.generate_sentence()
+"""
+print b.bigrams
+print b.get_probability(("twice","twice"), True)
+print b.get_probability((".","once"),True)
+"""
